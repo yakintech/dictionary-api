302b518eb3a7f09824d05f848e97a7d8be1e0e2e
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-06-14 14:28:32 +0300

    cors package added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { db } = require('./config/db');
+const cors = require('cors')
 const { adminUserRoutes } = require('./routes/adminUserRoute');
 const app = express();
 require('dotenv').config()
 app.use(express.json())
-
+app.use(cors());
 db.connect();
 
 app.use('/api/adminusers', adminUserRoutes)
